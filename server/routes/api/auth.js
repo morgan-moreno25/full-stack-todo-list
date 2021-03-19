3148ba2a10fcd3296b8e47dd21c0b5ab5f21dfc6
Morgan <morgan.moreno25@gmail.com>
2021-03-19 14:31:22 -0500

    Updated Auth Routes
    Changed login route from a get request to post request

--- server/routes/api/auth.js
+++ server/routes/api/auth.js
@@ -3,7 +3,7 @@ const router = require('express').Router();
 const authController = require('../../controllers/auth.controller');
 const { authenticate } = require('../../utils/middleware');
 
-router.get('/login', authController.login);
+router.post('/login', authController.login);
 router.post('/register', authController.register);
 router.get('/user', authenticate, authController.loadUser);
 
