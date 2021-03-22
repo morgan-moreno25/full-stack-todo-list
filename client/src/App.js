2a5fefd893e1c645a5d2e22336dc96bd3218a14d
Morgan <morgan.moreno25@gmail.com>
2021-03-22 15:59:57 -0500

    Update App.js
    Added a useEffect hook to check if the user isAuthenticated and fetches their data if true

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getAllProjects } from './redux/slices/project.slice';
+import { getAllTodos } from './redux/slices/todo.slice';
 
 import './App.scss';
 import AuthForm from './components/auth/AuthForm';
@@ -9,8 +11,15 @@ import Alert from './components/common/Alert';
 import Content from './components/content';
 
 export default function App() {
+	const dispatch = useDispatch();
 	const { isAuthenticated } = useSelector(state => state.auth);
 
+	useEffect(() => {
+		if (isAuthenticated) {
+			dispatch(getAllProjects());
+			dispatch(getAllTodos());
+		}
+	}, [isAuthenticated, dispatch]);
 	return (
 		<div id='app'>
 			<Alert />
