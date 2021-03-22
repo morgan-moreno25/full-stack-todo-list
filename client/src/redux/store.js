8bb4e6aa9abaaad90e0d78c81073b126f710cf1b
Morgan <morgan.moreno25@gmail.com>
2021-03-22 15:58:13 -0500

    Added Reducers to Store
    Added the project and todo reducers

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -2,10 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 
 import alertReducer from './slices/alert.slice';
 import authReducer from './slices/auth.slice';
+import projectReducer from './slices/project.slice';
+import todoReducer from './slices/todo.slice';
 
 export default configureStore({
 	reducer: {
 		alert: alertReducer,
 		auth: authReducer,
+		project: projectReducer,
+		todo: todoReducer,
 	},
 });
