f1056923232d588f50904163aa462d3315173985
Morgan <morgan.moreno25@gmail.com>
2021-03-22 16:01:31 -0500

    Update Sidenav
    Updated component to display each of the users' projects

--- client/src/components/layout/Sidenav.jsx
+++ client/src/components/layout/Sidenav.jsx
@@ -1,5 +1,28 @@
-import { Navbar } from 'react-bootstrap';
+import { useSelector, useDispatch } from 'react-redux';
+import { setCurrentProject } from '../../redux/slices/project.slice';
+
+import { Navbar, Button } from 'react-bootstrap';
+import AddProjectModal from '../content/AddProjectModal';
 
 export default function Sidenav() {
-	return <Navbar variant='light' id='nav'></Navbar>;
+	const dispatch = useDispatch();
+
+	const projects = useSelector(state => state.project.data);
+	const currentProject = useSelector(state => state.project.currentProject);
+
+	return (
+		<Navbar variant='light' id='nav'>
+			<ul>
+				{projects.map(project => (
+					<li
+						className={`${project === currentProject ? 'active' : ''} project`}
+						key={project.id}
+						onClick={() => dispatch(setCurrentProject({ project }))}>
+						{project.title}
+					</li>
+				))}
+			</ul>
+			<AddProjectModal />
+		</Navbar>
+	);
 }
