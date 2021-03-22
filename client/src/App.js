a186c364d12cd5f2a05d0038081e850446c97af9
Morgan <morgan.moreno25@gmail.com>
2021-03-22 12:59:46 -0500

    Added Authentication Logic
    Added the logic for the application to determine whether the Content or Login form is rendered

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,22 @@
 import React from 'react';
-
-import { Button, Container } from 'react-bootstrap';
+import { useSelector } from 'react-redux';
 
 import './App.scss';
 import AuthForm from './components/auth/AuthForm';
 import Sidenav from './components/layout/Sidenav';
 import Header from './components/layout/Header';
 import Alert from './components/common/Alert';
+import Content from './components/content';
 
 export default function App() {
+	const { isAuthenticated } = useSelector(state => state.auth);
+
 	return (
 		<div id='app'>
 			<Alert />
 			<Sidenav />
 			<Header />
-			<main id='content'>
-				<AuthForm />
-			</main>
+			<main id='content'>{isAuthenticated ? <Content /> : <AuthForm />}</main>
 		</div>
 	);
 }
