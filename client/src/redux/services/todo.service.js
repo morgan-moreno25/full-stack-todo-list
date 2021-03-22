2d6bf8bc92f6e9d73931072525cc8d1cba6db8b4
Morgan <morgan.moreno25@gmail.com>
2021-03-22 13:46:16 -0500

    Add TodoSlice to Redux Store

---
+++ client/src/redux/services/todo.service.js
@@ -0,0 +1,47 @@
+import axios from 'axios';
+
+const TodoService = (() => {
+	const url = '/api/todos';
+
+	const getAllTodos = async config => {
+		try {
+			const response = await axios.get(url, config);
+			return response.data;
+		} catch (error) {
+			return error.response.data;
+		}
+	};
+	const addTodo = async (body, config) => {
+		try {
+			const response = await axios.post(url, body, config);
+			return response.data;
+		} catch (error) {
+			return error.response.data;
+		}
+	};
+	const updateTodo = async (id, body, config) => {
+		try {
+			const response = await axios.put(`${url}/${id}`, body, config);
+			return response.data;
+		} catch (error) {
+			return error.response.data;
+		}
+	};
+	const deleteTodo = async (id, config) => {
+		try {
+			const response = await axios.delete(`${url}/${id}`, config);
+			return response.data;
+		} catch (error) {
+			return error.response.data;
+		}
+	};
+
+	return {
+		addTodo,
+		deleteTodo,
+		getAllTodos,
+		updateTodo,
+	};
+})();
+
+export default TodoService;
---
+++ client/src/redux/slices/todo.slice.js
@@ -0,0 +1,130 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { useTokenConfig } from '../utils';
+import todoService from '../services/todo.service';
+
+export const getAllTodos = createAsyncThunk('todo/getAll', async (_, thunkAPI) => {
+	const config = useTokenConfig(thunkAPI.getState);
+
+	try {
+		const data = await todoService.getAllTodos(config);
+		return {
+			todos: data.todos,
+		};
+	} catch (error) {
+		return thunkAPI.rejectWithValue(error);
+	}
+});
+export const addTodo = createAsyncThunk(
+	'todo/add',
+	async ({ text, priority, dueDate, project }, thunkAPI) => {
+		const body = JSON.stringify({ text, priority, dueDate, project });
+		const config = useTokenConfig(thunkAPI.getState);
+
+		try {
+			const data = await todoService.addTodo(body, config);
+			return {
+				todo: data.todo,
+			};
+		} catch (error) {
+			return thunkAPI.rejectWithValue(error);
+		}
+	}
+);
+export const updateTodo = createAsyncThunk(
+	'todo/update',
+	async ({ id, todo: { text, priority, dueDate, project } }, thunkAPI) => {
+		const body = JSON.stringify({ text, priority, dueDate, project });
+		const config = useTokenConfig(thunkAPI.getState);
+
+		try {
+			const data = await todoService.updateTodo(id, body, config);
+			return {
+				id,
+				updatedTodo: data.updatedTodo,
+			};
+		} catch (error) {
+			return thunkAPI.rejectWithValue(error);
+		}
+	}
+);
+export const deleteTodo = createAsyncThunk('todo/delete', async ({ id }, thunkAPI) => {
+	const config = useTokenConfig(thunkAPI.getState);
+
+	try {
+		const data = await todoService.deleteTodo(id, config);
+		return {
+			id,
+			message: data.message,
+		};
+	} catch (error) {
+		return thunkAPI.rejectWithValue(error);
+	}
+});
+
+const todoSlice = createSlice({
+	name: 'todo',
+	initialState: {
+		data: [],
+		currentTodo: {},
+		isLoading: false,
+		error: null,
+	},
+	reducers: {
+		setCurrentTodo: (state, { payload }) => {
+			state.currentTodo = payload.todo;
+		},
+	},
+	extraReducers: $ => {
+		$.addCase(getAllTodos.pending, state => {
+			state.isLoading = true;
+		});
+		$.addCase(addTodo.pending, state => {
+			state.isLoading = true;
+		});
+		$.addCase(updateTodo.pending, state => {
+			state.isLoading = true;
+		});
+		$.addCase(deleteTodo.pending, state => {
+			state.isLoading = true;
+		});
+		$.addCase(getAllTodos.fulfilled, (state, { payload }) => {
+			state.data = payload.todos;
+			state.isLoading = false;
+		});
+		$.addCase(addTodo.fulfilled, (state, { payload }) => {
+			state.data.push(payload.todo);
+			state.isLoading = false;
+		});
+		$.addCase(updateTodo.fulfilled, (state, { payload }) => {
+			const index = state.data.findIndex(todo => todo.id === payload.id);
+			if (index !== -1) {
+				state.data.splice(index, 1, payload.updatedTodo);
+			}
+			state.isLoading = false;
+		});
+		$.addCase(deleteTodo.fulfilled, (state, { payload }) => {
+			state.data = state.data.filter(todo => todo.id !== payload.id);
+			state.isLoading = false;
+		});
+		$.addCase(getAllTodos.rejected, (state, { payload }) => {
+			state.isLoading = false;
+			state.error = payload;
+		});
+		$.addCase(addTodo.rejected, (state, { payload }) => {
+			state.isLoading = false;
+			state.error = payload;
+		});
+		$.addCase(updateTodo.rejected, (state, { payload }) => {
+			state.isLoading = false;
+			state.error = payload;
+		});
+		$.addCase(deleteTodo.rejected, (state, { payload }) => {
+			state.isLoading = false;
+			state.error = payload;
+		});
+	},
+});
+
+export const { setCurrentTodo } = todoSlice.actions;
+
+export default todoSlice.reducer;
