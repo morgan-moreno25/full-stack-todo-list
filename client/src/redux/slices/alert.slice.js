887f8e44516711278df4f0ef6375bcd32f2c7f52
Morgan <morgan.moreno25@gmail.com>
2021-03-22 11:04:43 -0500

    Added Alert Slice to Redux Store

---
+++ client/src/redux/slices/alert.slice.js
@@ -0,0 +1,22 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const alertSlice = createSlice({
+	name: 'alert',
+	initialState: {
+		message: '',
+		type: '',
+	},
+	reducers: {
+		updateAlert: (state, { payload }) => {
+			state.message = payload.message;
+			state.type = payload.type;
+		},
+		clearAlert: state => {
+			state.error = '';
+			state.message = '';
+			state.type = '';
+		},
+	},
+});
+
+export default alertSlice.reducer;
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,8 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
+
+import alertReducer from './slices/alert.slice';
 import authReducer from './slices/auth.slice';
 
 export default configureStore({
 	reducer: {
+		alert: alertReducer,
 		auth: authReducer,
 	},
 });
