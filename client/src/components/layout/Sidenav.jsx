4a0276ad68f0ad85bf07d303036bb17e4855f2a6
Morgan <morgan.moreno25@gmail.com>
2021-03-24 17:04:58 -0500

    Updated Sidenav
    Updated so that the modal buttons will not be displayed unless a user is logged in

--- client/src/components/layout/Sidenav.jsx
+++ client/src/components/layout/Sidenav.jsx
@@ -8,25 +8,32 @@ import AddTodoModal from '../content/modals/AddTodoModal';
 export default function Sidenav() {
 	const dispatch = useDispatch();
 
+	const isAuthenticated = useSelector(state => state.auth.isAuthenticated);
 	const projects = useSelector(state => state.project.data);
 	const currentProject = useSelector(state => state.project.currentProject);
 
 	return (
 		<Navbar variant='light' id='nav'>
-			<ul>
-				{projects.map(project => (
-					<li
-						className={`${project === currentProject ? 'active' : ''} project`}
-						key={project.id}
-						onClick={() => dispatch(setCurrentProject({ project }))}>
-						{project.title}
-					</li>
-				))}
-			</ul>
-			<div id='add-buttons'>
-				<AddProjectModal />
-				<AddTodoModal />
-			</div>
+			{isAuthenticated ? (
+				<>
+					<ul>
+						{projects.map(project => (
+							<li
+								className={`${project === currentProject ? 'active' : ''} project`}
+								key={project.id}
+								onClick={() => dispatch(setCurrentProject({ project }))}>
+								{project.title}
+							</li>
+						))}
+					</ul>
+					<div id='add-buttons'>
+						<AddProjectModal />
+						<AddTodoModal />
+					</div>
+				</>
+			) : (
+				<p>Login to view this feature</p>
+			)}
 		</Navbar>
 	);
 }
