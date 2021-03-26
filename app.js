ce9533efb8cd3056bb8e0f30fc4ea46d013cbb80
Morgan <morgan.moreno25@gmail.com>
2021-03-26 10:14:36 -0500

    Prepare to Deploy

--- app.js
+++ app.js
@@ -14,4 +14,12 @@ app.use(requestLogger);
 
 app.use('/', require('./routes'));
 
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
+
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
+
 module.exports = app;
